cf64597e4410baa0c0cfd841c241d533008b49b4
Andrei Leonard Nicusan <a.l.nicusan@bham.ac.uk>
2021-12-22 09:23:23 +0000

    build in home/lethe/dealii

--- build_script.sh
+++ build_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 logfile=build_script.log
 printf "\n-\nStarting compilation run on $(date)\n-\n" >> $logfile
-./candi.sh -j 8 --prefix=/home/lethe/dealii/build -y |& tee -a $logfile
+./candi.sh -j 8 --prefix=/home/lethe/dealii -y |& tee -a $logfile
